fba175970afb6726b38554702bdfad6fc0fb2ace
AmmaarSalie <s227141865@mandela.ac.za>
2025-09-02 12:54:09 +0200

    Added DashboardCaregiver with layout

---
+++ app/src/main/java/com/example/re_collectui/DashboardCaregiver.java
@@ -0,0 +1,40 @@
+package com.example.re_collectui;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DashboardCaregiver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard_caregiver);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        SharedPreferences sharedPref = getSharedPreferences("userSession", MODE_PRIVATE);
+        int patientID = sharedPref.getInt("patientID", -1); // for patientID for session
+    }
+
+    public void on_Caregiver_profile_Click(View view) {
+
+        }
+
+    public void on_Caregiver_patients_Click(View view) {
+    }
+
+    public void on_Caregiver_requests_Click(View view) {
+    }
+}
