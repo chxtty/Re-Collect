3ff256a7d9d280b7f7c8a955875657aeceff2595
JRodze <kjoeevin2@gmail.com>
2025-10-27 15:16:20 +0200

    activity_border_feature

--- app/src/main/java/com/example/re_collectui/GlobalVars.java
+++ app/src/main/java/com/example/re_collectui/GlobalVars.java
@@ -2,6 +2,6 @@ package com.example.re_collectui;
 
 public class GlobalVars {
    // public static String apiPath = "http://10.0.2.2/recollect/api.php?action=";
-   //public static String apiPath = "http://100.79.152.109/android/api.php?action=";
-   public static String apiPath = "http://100.104.224.68/android/api.php?action=";
+   public static String apiPath = "http://100.79.152.109/android/api.php?action=";
+   //0.public static String apiPath = "http://100.104.224.68/android/api.php?action=";
 }
